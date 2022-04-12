b1d367749ca64825b57fde22a0645fabf352a093
geolffreym <gmjun2000@gmail.com>
2022-04-12 17:32:46 -0600

    chore: add perf metrics

--- main.go
+++ main.go
@@ -12,29 +12,51 @@ package main
 
 import (
 	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
 
 	IO "github.com/geolffreym/rolling-sync/fileio"
 	Sync "github.com/geolffreym/rolling-sync/sync"
 )
 
 func main() {
-	// Read file to split in chunks
+
+	// Performance test
+	cpufile, err := os.Create("cpu.pprof")
+	err = pprof.StartCPUProfile(cpufile)
+	if err != nil {
+		panic(err)
+	}
+
+	defer cpufile.Close()
+	defer pprof.StopCPUProfile()
+
 	blockSize := 1 << 4 // 16 bytes
 	io := IO.New(blockSize)
 	sync := Sync.New(blockSize)
 
 	// Memory performance improvement using bufio.Reader
 	reader, err := io.Open("mock.txt")
-	if err != nil {
-		log.Fatal(err)
-		return
+	newFile, err := io.Open("mockV2.txt")
+
+	for i := 0; i <= 100000; i++ {
+
+		// For each block slice from file
+		sync.FillTable(reader)
+		signatures := sync.Signatures()
+		// TODO write delta if needed
+		sync.Delta(signatures, newFile)
 	}
 
-	// For each block slice from file
-	sync.FillTable(reader)
-	signatures := sync.Signatures()
-	newFile, err := io.Open("mockV2.txt")
-	// TODO write delta if needed
-	sync.Delta(signatures, newFile)
+	f, err := os.Create("mem.proof")
+	if err != nil {
+		log.Fatal("could not create memory profile: ", err)
+	}
+	defer f.Close() // error handling omitted for example
+	runtime.GC()    // get up-to-date statistics
+	if err := pprof.WriteHeapProfile(f); err != nil {
+		log.Fatal("could not write memory profile: ", err)
+	}
 
 }
