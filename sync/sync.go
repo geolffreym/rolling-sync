a3139be6c8706e210b0f3a3fcea6947299e38f20
Geolffrey Mena <gmjun2000@gmail.com>
2022-04-13 09:37:40 -0600

    Update sync.go

--- sync/sync.go
+++ sync/sync.go
@@ -51,7 +51,7 @@ func New(size int) *Sync {
 }
 
 // Fill signature from blocks
-// Weak + Strong hash table to avoid collisions
+// Weak + Strong hash table to avoid collisions + perf
 func (s *Sync) FillTable(reader *bufio.Reader) {
 
 	for {
@@ -152,6 +152,7 @@ func (s *Sync) Delta(signatures []Table, reader *bufio.Reader) map[int]*Bytes {
 
 		// Add new el to checksum
 		s.w.RollIn(c)
+                // Keep moving forward if not data ready to analize
 		if s.w.Count() < s.blockSize {
 			continue
 		}
